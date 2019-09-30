41d659b39118445c3782197e06f28b0806e81a45
25447 <shiyf286@chinaunicom.cn>
2019-09-30 11:43:51 +0800

    Initial commit

---
+++ src/main/java/com/yifanshi/splitnewproject/SplitnewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.yifanshi.splitnewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitnewprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitnewprojectApplication.class, args);
+    }
+
+}
